c605d7d07dd9daa0b3846bce0cd67c60e962d343
spotlessmind <vladimir.bezkorovainy@gmail.com>
2014-04-28 19:29:06 +0200

    Initial version
    Create static library which keep examples and add unite test project for
    it

---
+++ code_playground_tests/skip_spaces_test.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace code_playground_tests
+{		
+	TEST_CLASS(skip_spaces_test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
